b1b3596d132996de5b70973993386aa38afc8e3d
Rafael Herrera <rafaelhecr@protonmail.com>
2018-08-29 16:20:01 -0500

    Inicio de la creación del crud

---
+++ app.js
@@ -0,0 +1,43 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const index = require('./routes/index');
+const personaForm = require('./routes/personaForm');
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', personaForm);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  let err = new Error('Not found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
